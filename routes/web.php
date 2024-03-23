0d156cc23b5f27cf27159ded5c1527d388e247e7
dhouha belakhel <dhouhabelakhel2001@gmail.com>
2024-03-23 13:01:09 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,70 @@
+<?php
+use App\Http\Controllers\ClientController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TechnicienController;
+use App\Http\Controllers\InterventionController;
+use App\Http\Controllers\AuthController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::post('signup', [TechnicienController::class,'addTechniciens']);
+Route::post('login', [AuthController::class,'login']);
+
+
+Route::resource('clients', ClientController::class);
+
+//get all the clients
+Route::get('clients', [ClientController::class, 'getClients']);
+//addclient
+Route::post('addClients', [ClientController::class, 'addClients']);
+//update client
+Route::put('updateClients/{Code}',[ClientController::class,'updateClients']);
+//delete client
+Route::delete('deleteClients/{Code}',[ClientController::class,'deleteClients']);
+//bycode
+Route::get('clients/{code}', [ClientController::class, 'show']);
+
+//technicien
+
+Route::resource('techniciens', TechnicienController::class);
+//get all tchnicien
+Route::get('techniciens',[TechnicienController::class,'getTechniciens']);
+//addtechnicien
+Route::post('addTechniciens', [TechnicienController::class, 'addTechniciens']);
+//delete client
+Route::delete('deleteTechniciens/{Matricule}',[TechnicienController::class,'deleteTechniciens']);
+//bycode
+Route::get('techniciens/{Matricule}', [TechnicienController::class, 'show']);
+//update technicien
+Route::put('updateTechniciens/{Matricule}',[TechnicienController::class,'updateTechniciens']);
+
+
+//intervention
+Route::resource('interventions', InterventionController::class);
+
+
+//get all the interventions
+Route::get('/interventions',[InterventionController::class,'getInterventions']);
+//add interventions
+Route::post('addInterventions', [InterventionController::class,'addInterventions']);
+//delete intervention
+Route::delete('deleteInterventions/{matricule}',[InterventionController::class,'deleteInterventions']);
+//bycode
+Route::get('interventions/{Num}', [InterventionController::class, 'show']);
+//update technicien
+Route::put('updateInterventions/{Num}',[InterventionController::class,'updateInterventions']);
+
+
+Route::post('/add-clients', [ClientController::class, 'addClients'])->name('addClients');
+
